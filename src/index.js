f3ac63d6028d270b96a27954ffa8c4218d830d5c
Saad Patel <patelsaadn@gmail.com>
2020-06-15 02:06:23 +0530

    Add Routes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,37 @@
 let express = require("express")
-
+let path =require('path')
 let app =express()
 
+
+let studentRoute = require('./routes/student')
+
+
+app.use((req,res,nextFunc)=>{
+    console.log("For proper sequence stuff, ...")
+    console.log(`Hey the requested URl is ${req.originalUrl} and the time and date are  ${new Date().toString()}`)
+
+    nextFunc()
+
+})
+
+
+app.use(studentRoute)
 app.use(express.static('public'))
+
+
+
+// To manage not found pages: 404
+app.use((req,res,nextFunc) =>{
+    res.status(404).send("Hey buddy... I think you should watch Dark releasing on 27th June.... By the way You are Lost. 404")
+})
+
+// To manage not found pages: 500
+
+app.use((err,req,res,nextFunc) =>{
+console.error(err.stack)
+
+res.sendFile(path.join(__dirname, '../public/500.html'))
+})
 
 const PORT = 5140
 
--- src/routes/student.js
+++ src/routes/student.js
@@ -2,6 +2,20 @@ let express =require('express')
 let router = express.Router()
 
 
-router.get('/student')
+router.get('/student' , (req, res) =>{
+    res.send(" We would like to know about the student")
+})
+
+router.get('/student/:name' , (req, res) =>{
+    res.send(`We would like to know about the student named as ${req.params.name}`)
+})
+
+router.get('/student/:rollNumber' , (req, res) =>{
+    res.send(`We would like to know about the student with roll number  ${req.params.rollNumber}`)
+})
+
+router.get('/error' , (req, res) =>{
+        throw new Error('This is a forced error')
+})
 
 module.exports = router
